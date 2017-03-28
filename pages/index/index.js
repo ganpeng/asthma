e66bc784a583f64012ca852c15f6fd82d2f8db31
louisGan <louisGan1988@gmail.com>
2017-03-28 16:29:29 +0800

    初始化小程序

---
+++ pages/index/index.js
@@ -0,0 +1,27 @@
+//index.js
+//获取应用实例
+const url = '../logs/logs';
+
+
+var app = getApp()
+Page({
+  data: {
+    motto: '你好，微信小程序',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap() {
+    wx.navigateTo({ url })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
