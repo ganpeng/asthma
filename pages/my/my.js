1ab1e729c5248c0620429532a896e0fa93612e21
louisGan <louisGan1988@gmail.com>
2017-03-29 16:16:38 +0800

    我的页面完成

---
+++ pages/my/my.js
@@ -0,0 +1,48 @@
+const app = getApp();
+
+Page({
+  data: {
+    userInfo: {},
+    // 页面的宽高属性
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换
+    currentTab: 0,
+  },
+  onLoad: function () {
+      wx.pro.getSystemInfo()
+        .then((res) => {
+            this.setData( {
+              winWidth: res.windowWidth,
+              winHeight: res.windowHeight
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    wx.pro.getUserInfo()
+        .then((userInfo) => {
+            this.setData({ userInfo });
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+  },
+
+  bindChange(e) {
+      const that = this;
+      that.setData({ currentTab: e.detail.current });
+  },
+
+  switchNav(e) {
+      const that = this;
+      if (this.data.currentTab === e.target.dataset.current ) {
+          return false;
+      } else {
+          that.setData({
+              currentTab: e.target.dataset.current
+          })
+      }
+  }
+})
