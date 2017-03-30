76fe08dabba5c6238831ce6ba1695e70fa057264
louisGan <louisGan1988@gmail.com>
2017-03-30 13:43:11 +0800

    提交评分

--- pages/coursedetail/index.js
+++ pages/coursedetail/index.js
@@ -3,64 +3,62 @@ const starActive = '../../images/star_gan_active.png';
 
 var app = getApp()
 Page({
-  data: {
-    userInfo: {},
-    starImages: [],
-    // 页面的宽高属性
-    winWidth: 0,
-    winHeight: 0,
-    // tab切换
-    currentTab: 0,
-  },
-  onLoad: function () {
+    data: {
+        userInfo: {},
+        starImages: [],
+        // 页面的宽高属性
+        winWidth: 0,
+        winHeight: 0,
+        // tab切换
+        currentTab: 0
+    },
+    onLoad: function() {
 
-      this.setImages();
+        this.setImages();
 
-      wx.pro.getSystemInfo()
-        .then((res) => {
-            this.setData( {
-              winWidth: res.windowWidth,
-              winHeight: res.windowHeight
-            });
-        })
-        .catch((err) => {
+        wx.pro.getSystemInfo().then((res) => {
+            this.setData({winWidth: res.windowWidth, winHeight: res.windowHeight});
+        }).catch((err) => {
             console.log(err);
         })
-  },
-  setImages() {
-      const starImages = [];
-      for (let i = 0; i < 5; i++) {
-          starImages.push(star);
-      }
-      this.setData({starImages});
-  },
-  bindChange(e) {
-      const that = this;
-      that.setData({ currentTab: e.detail.current });
-  },
+    },
+    setImages() {
+        const starImages = [];
+        for (let i = 0; i < 5; i++) {
+            starImages.push(star);
+        }
+        this.setData({starImages});
+    },
+    bindChange(e) {
+        const that = this;
+        that.setData({currentTab: e.detail.current});
+    },
 
-  switchNav(e) {
-      const that = this;
-      if (this.data.currentTab === e.target.dataset.current ) {
-          return false;
-      } else {
-          that.setData({
-              currentTab: e.target.dataset.current
-          })
-      }
-  },
-  changeStar(e) {
-      const starImages = [];
-      const index = e.target.dataset.gindex;
-      const that = this;
-      console.log(index);
-      for (let i = 0; i < 5; i++) {
-          if (i <= index) {
-              starImages.push(starActive);
-          } else {
-              starImages.push(star);
-          }
-      }
-      that.setData({starImages});
-  }
+    switchNav(e) {
+        const that = this;
+        if (this.data.currentTab === e.target.dataset.current) {
+            return false;
+        } else {
+            that.setData({currentTab: e.target.dataset.current})
+        }
+    },
+    changeStar(e) {
+        const starImages = [];
+        const index = e.target.dataset.gindex;
+        const that = this;
+        for (let i = 0; i < 5; i++) {
+            if (i <= index) {
+                starImages.push(starActive);
+            } else {
+                starImages.push(star);
+            }
+        }
+        that.setData({starImages});
+    },
+
+    submitRating() {
+        const that = this;
+        const rating = that.data.starImages.filter((image) => image === starActive).length;
+        console.log(rating);
+    }
 })
