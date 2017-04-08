025360ae3da56715ea9cc8e8fe52f4b9fb67602e
Rem Zolotykh <rem@zolotykh.net>
2017-04-08 10:09:50 +0800

    恢复

--- server/config/index.js
+++ server/config/index.js
@@ -1,7 +1,7 @@
 export default {
     // db: "mongodb://localhost/xhnj",
-    db : "mongodb://192.168.0.163:27017/xhnj",
-    // db: "mongodb://xhnj:xhnj123@101.200.228.228:27017/xhnj",
+    //db : "mongodb://192.168.0.163:27017/xhnj",
+    db: "mongodb://xhnj:xhnj123@101.200.228.228:27017/xhnj",
     // options
 }
 
